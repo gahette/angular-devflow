5e3ee0e1b2c396f853e391d93f492ba5ad8304a1
gahette <gahette@icloud.com>
2026-04-01 17:45:37 +0200

    Assets & Metadata

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.html',
+    styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('angular-devflow');
+    // protected readonly title = signal('angular-devflow');
+    private title = inject(Title);
+    private meta = inject(Meta);
+
+    constructor() {
+        this.title.setTitle('AngularDevflow');
+        this.meta.updateTag({
+            name: 'description',
+            content:
+                ' A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
+        });
+    }
 }
